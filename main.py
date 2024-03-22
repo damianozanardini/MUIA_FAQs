414feff77391ee1f3e011f9e488d697d73ed2c40
Damiano Zanardini <damiano.zanardini@gmail.com>
2024-03-22 10:03:44 +0100

    Deactivating the sidebar

--- main.py
+++ main.py
@@ -25,20 +25,24 @@ import os
 
 st.set_page_config(layout = "wide")
 
-#with st.sidebar:
-#    DOCS_DIR = os.path.abspath("./uploaded_docs")
-#    if not os.path.exists(DOCS_DIR):
-#        os.makedirs(DOCS_DIR)
-#    st.subheader("Add to the Knowledge Base")
-#    with st.form("my-form", clear_on_submit=True):
-#        uploaded_files = st.file_uploader("Upload a file to the Knowledge Base:", accept_multiple_files = True)
-#        submitted = st.form_submit_button("Upload!")
-#
-#    if uploaded_files and submitted:
-#        for uploaded_file in uploaded_files:
-#            st.success(f"File {uploaded_file.name} uploaded successfully!")
-#            with open(os.path.join(DOCS_DIR, uploaded_file.name),"wb") as f:
-#                f.write(uploaded_file.read())
+USE_SIDEBAR = False # The sidebar is for developing purposes only
+MODEL = "mixtral_8x7b" # "ai-llama2-70b"
+
+DOCS_DIR = os.path.abspath("./uploaded_docs")
+if not os.path.exists(DOCS_DIR):
+    os.makedirs(DOCS_DIR)
+
+if USE_SIDEBAR:
+    with st.sidebar:
+        st.subheader("Add to the Knowledge Base")
+        with st.form("my-form", clear_on_submit=True):
+            uploaded_files = st.file_uploader("Upload a file to the Knowledge Base:", accept_multiple_files = True)
+            submitted = st.form_submit_button("Upload!")
+        if uploaded_files and submitted:
+            for uploaded_file in uploaded_files:
+                st.success(f"File {uploaded_file.name} uploaded successfully!")
+                with open(os.path.join(DOCS_DIR, uploaded_file.name),"wb") as f:
+                    f.write(uploaded_file.read())
 
 ############################################
 # Component #2 - Embedding Model and LLM
@@ -61,9 +65,12 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.vectorstores import FAISS
 import pickle
 
-with st.sidebar:
-    # Option for using an existing vector store
-    use_existing_vector_store = st.radio("Use existing vector store if available", ["Yes", "No"], horizontal=True)
+if USE_SIDEBAR:
+    with st.sidebar:
+        # Option for using an existing vector store
+        use_existing_vector_store = st.radio("Use existing vector store if available", [True, False], horizontal=True)
+else:
+    use_existing_vector_store = True
 
 # Path to the vector store file
 vector_store_path = "vectorstore.pkl"
@@ -75,27 +82,38 @@ raw_documents = DirectoryLoader(DOCS_DIR).load()
 # Check for existing vector store file
 vector_store_exists = os.path.exists(vector_store_path)
 vectorstore = None
-if use_existing_vector_store == "Yes" and vector_store_exists:
+if use_existing_vector_store and vector_store_exists:
     with open(vector_store_path, "rb") as f:
         vectorstore = pickle.load(f)
-    with st.sidebar:
-        st.success("Existing vector store loaded successfully.")
+    if USE_SIDEBAR:
+        with st.sidebar:
+            st.success("Existing vector store loaded successfully.")
 else:
-    with st.sidebar:
+    if USE_SIDEBAR:
+        with st.sidebar:
+            if raw_documents:
+                with st.spinner("Splitting documents into chunks..."):
+                    text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+                    documents = text_splitter.split_documents(raw_documents)
+
+                with st.spinner("Adding document chunks to vector database..."):
+                    vectorstore = FAISS.from_documents(documents, document_embedder)
+
+                with st.spinner("Saving vector store"):
+                    with open(vector_store_path, "wb") as f:
+                        pickle.dump(vectorstore, f)
+                st.success("Vector store created and saved.")
+            else:
+                st.warning("No documents available to process!", icon="⚠️")
+    else:
+        st.write("XXX")
         if raw_documents:
-            with st.spinner("Splitting documents into chunks..."):
-                text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
-                documents = text_splitter.split_documents(raw_documents)
-
-            with st.spinner("Adding document chunks to vector database..."):
-                vectorstore = FAISS.from_documents(documents, document_embedder)
-
-            with st.spinner("Saving vector store"):
-                with open(vector_store_path, "wb") as f:
-                    pickle.dump(vectorstore, f)
-            st.success("Vector store created and saved.")
-        else:
-            st.warning("No documents available to process!", icon="⚠️")
+            st.write("XXX")
+            text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+            documents = text_splitter.split_documents(raw_documents)
+            vectorstore = FAISS.from_documents(documents, document_embedder)
+            with open(vector_store_path, "wb") as f:
+                pickle.dump(vectorstore, f)
 
 ############################################
 # Component #4 - LLM Response Generation and Chat
@@ -118,8 +136,7 @@ prompt_template = ChatPromptTemplate.from_messages(
     [("system", "Te llamas MUIAbot, y eres un asistente basado en IA. Siempre contestarás a las preguntas en Español y solo basándote en el contexto. Si alguna preguntas está fuera de contexto, dirás amablemente que no puedes contestar."), ("user", "{input}")]
 )
 user_input = st.chat_input("Escribe aquí cualquier pregunta sobre el MUIA. Intenta ser preciso.")
-llm = ChatNVIDIA(model="mixtral_8x7b")
-#llm = ChatNVIDIA(model="ai-llama2-70b")
+llm = ChatNVIDIA(model=MODEL)
 
 chain = prompt_template | llm | StrOutputParser()
 
